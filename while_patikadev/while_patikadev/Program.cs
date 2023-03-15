693df3c4e0c40d07429d4eceda4f6df2a7f60f3f
GOKHANDURUR <123666133+GOKHANDURUR@users.noreply.github.com>
2023-03-15 13:02:23 +0300

    while çalışması eklendi

---
+++ while_patikadev/while_patikadev/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace while_patikadev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayac = 1;
+            while (sayac <= 5)
+            {
+                Console.WriteLine(sayac);
+                sayac++;
+            }
+            int[] sayiDizisi = { 2, 4, 5, 1, 2, 4 };
+            int toplam = 0;
+
+            foreach (int i in sayiDizisi)
+            {
+                toplam += i;
+            }
+            Console.ReadKey();
+        }
+    }
+}
